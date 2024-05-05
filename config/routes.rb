23ac35e180de1de0b248ab1fad11097b8bf544cf
Apple <apple@Apples-MacBook-Air.local>
2024-05-06 03:28:25 +0900

    [fix]コントローラ名、モデル名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :new, :create, :show,  :edit, :update, :destroy]
+  #localhost:3000/users
 end
